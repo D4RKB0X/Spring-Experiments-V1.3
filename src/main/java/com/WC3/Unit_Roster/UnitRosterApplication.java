0d71ee47b6dbd13620808e4d8824962cfeedb91c
Lain Iwakura <kobeangelovillegas12@gmail.com>
2026-05-14 17:16:00 +0800

    First commit, pre-prep files

---
+++ src/main/java/com/WC3/Unit_Roster/UnitRosterApplication.java
@@ -0,0 +1,11 @@
+package com.WC3.Unit_Roster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitRosterApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UnitRosterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/WC3/Unit_Roster/UnitRosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WC3.Unit_Roster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitRosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
